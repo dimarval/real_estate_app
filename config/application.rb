02fd5d9beb75402ebffa8e294f8b6fff47038cc1
Diego Martínez <dmvaldelamar@gmail.com>
2018-03-28 15:45:08 -0600

    Define traslations files hierarchy

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module RealEstateApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Internationalization and localization config
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
